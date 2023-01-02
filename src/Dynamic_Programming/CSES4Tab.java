d66ed2625a399657aa7e9cf6ded2f87ba5954ad8
manas <manasrai1234567@gmail.com>
2023-01-02 23:07:25 +0530

    DP Test commit 2.0

---
+++ src/Dynamic_Programming/CSES4Tab.java
@@ -0,0 +1,169 @@
+package Dynamic_Programming;
+
+import java.util.*;
+import java.io.*;
+
+/*
+Good one!!
+
+Consider a money system consisting of n coins. Each coin has a positive integer value. Your task is to calculate the number of distinct ordered ways you can produce a money sum x using the available coins.
+
+For example, if the coins are {2,3,5} and the desired sum is 9, there are 3 ways:
+2+2+5
+3+3+3
+2+2+2+3
+ */
+public class CSES4Tab {
+    static FastRead sc = new FastRead(System.in);
+    static PrintWriter out = new PrintWriter(System.out);
+    private static class FastRead {
+        private final InputStream stream;
+        private final byte[] buf = new byte[1024];
+        private int curChar, numChars;
+        public FastRead (InputStream stream) {
+            this.stream = stream;
+        }
+        public FastRead (String file) throws IOException {
+            this.stream = new FileInputStream (file);
+        }
+        void run() {
+            try {
+                PrintStream fs = new PrintStream("error.txt");
+                System.setErr(fs);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        public int cscan () throws IOException {
+            if (curChar >= numChars) {
+                curChar = 0;
+                numChars = stream.read (buf);
+            }
+            if (numChars == -1)
+                return numChars;
+            return buf[curChar++];
+        }
+        public int nextInt () throws IOException {
+            int c = cscan (), sgn = 1;
+            while (space (c))
+                c = cscan ();
+            if (c == '-') {
+                sgn = -1;
+                c = cscan ();
+            }
+            int res = 0;
+            do {
+                res = (res << 1) + (res << 3);
+                res += c - '0';
+                c = cscan ();
+            }
+            while (!space (c));
+            return res * sgn;
+        }
+        public String nextString () throws IOException {
+            int c = cscan ();
+            while (space (c))
+                c = cscan ();
+            StringBuilder res = new StringBuilder ();
+            do {
+                res.appendCodePoint (c);
+                c = cscan ();
+            }
+            while (!space (c));
+            return res.toString ();
+        }
+        public double nextDouble () throws IOException {
+            int c = cscan (), sgn = 1;
+            while (space (c))
+                c = cscan ();
+            if (c == '-') {
+                sgn = -1;
+                c = cscan ();
+            }
+            double res = 0;
+            while (!space (c) && c != '.') {
+                if (c == 'e' || c == 'E')
+                    return res * exp(10, nextInt ());
+                res *= 10;
+                res += c - '0';
+                c = cscan ();
+            }
+            if (c == '.') {
+                c = cscan ();
+                double m = 1;
+                while (!space (c)) {
+                    if (c == 'e' || c == 'E')
+                        return res * exp(10, nextInt ());
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = cscan ();
+                }
+            }
+            return res * sgn;
+        }
+        public long nextLong () throws IOException {
+            int c = cscan (), sgn = 1;
+            while (space (c))
+                c = cscan ();
+            if (c == '-') {
+                sgn = -1;
+                c = cscan ();
+            }
+            long res = 0;
+            do {
+                res = (res << 1) + (res << 3);
+                res += c - '0';
+                c = cscan ();
+            }
+            while (!space (c));
+            return res * sgn;
+        }
+        public boolean space (int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+    }
+    static final int mod = 1_000_000_007;
+    static long mul(long a, long b) {
+        return (a * b) % mod;
+    }
+    static long exp(long base, long exp) {
+        if (exp == 0) return 1;
+        long half = exp(base, exp / 2);
+        if (exp % 2 == 0) return mul(half, half);
+        return mul(half, mul(half, base));
+    }
+    static void debug(Object... o){
+        if(o.length != 0) System.err.println(Arrays.deepToString(o));
+        else System.err.println();
+    }
+    static final Random random = new Random();
+
+    static void ruffleSort(int[] a) {
+            int n = a.length;  //shuffle then sort to decrease worst case probability
+            for (int i = 0; i < n; i++) {
+                int oi = random.nextInt(n), temp = a[oi];
+                a[oi] = a[i];
+                a[i] = temp;
+            }
+            Arrays.sort(a);
+    }
+    public static void main(String[] args)throws IOException {
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int[] c = new int[n];
+        int i;
+        for (i = 0; i < n; i++) c[i] = sc.nextInt();
+        ruffleSort(c);
+        long[] dp=new long[x+1];
+        dp[0] = 1;
+        for(int coin: c){
+            for(i=1;i<=x;i++)
+                if(i-coin>=0){
+                dp[i] += dp[i-coin];
+                if(dp[i] > mod) dp[i] -= mod;
+            }
+        }
+        out.println(dp[x]%mod);
+        out.flush();
+    }
+}
